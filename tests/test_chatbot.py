b7b942e27ad03e019f741ef75556004786ed7acb
Jeremy Pinto <jerpint@gmail.com>
2023-03-03 16:43:59 -0500

    add OOD test

--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -108,6 +108,40 @@ def test_chatbot_real_data__chatGPT():
     assert isinstance(answer, str)
 
 
+def test_chatbot_real_data__chatGPT_OOD():
+    hf_transformers_cfg = BusterConfig(
+        documents_file=DOCUMENTS_FILE,
+        unknown_prompt="This doesn't seem to be related to the huggingface library. I am not sure how to answer.",
+        embedding_model="text-embedding-ada-002",
+        top_k=3,
+        thresh=0.7,
+        max_words=3000,
+        response_format="slack",
+        completer_cfg={
+            "name": "ChatGPT",
+            "text_before_prompt": (
+                """You are a slack chatbot assistant answering technical questions about huggingface transformers, a library to train transformers in python. """
+                """Make sure to format your answers in Markdown format, including code block and snippets. """
+                """Do not include any links to urls or hyperlinks in your answers. """
+                """If you do not know the answer to a question, or if it is completely irrelevant to the library usage, simply reply with: """
+                """'This doesn't seem to be related to the huggingface library.'\n"""
+                """For example:\n"""
+                """What is the meaning of life for huggingface?\n"""
+                """This doesn't seem to be related to the huggingface library.\n"""
+                """Now answer the following question:\n"""
+            ),
+            "text_before_documents": "Only use these documents as reference:\n",
+            "completion_kwargs": {
+                "model": "gpt-3.5-turbo",
+            },
+        },
+    )
+    buster = Buster(hf_transformers_cfg)
+    answer = buster.process_input("What is a good recipe for brocolli soup?")
+    assert isinstance(answer, str)
+    assert hf_transformers_cfg.unknown_prompt in answer
+
+
 def test_chatbot_real_data__GPT():
     hf_transformers_cfg = BusterConfig(
         documents_file=DOCUMENTS_FILE,
