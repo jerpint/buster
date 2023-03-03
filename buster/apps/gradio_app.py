1e24d85c5c2a0c635fbebeba602ffa734f272dab
Jeremy Pinto <jerpint@gmail.com>
2023-03-03 17:26:17 -0500

    update OOD prompt

--- buster/apps/gradio_app.py
+++ buster/apps/gradio_app.py
@@ -9,8 +9,6 @@ buster_cfg = BusterConfig(
     top_k=3,
     thresh=0.7,
     max_words=3000,
-    response_format="gradio",
-    response_footnote="I'm a bot 🤖 trained to answer huggingface 🤗 transformers questions. My answers aren't always perfect.",
     completer_cfg={
         "name": "ChatGPT",
         "text_before_prompt": (
@@ -18,11 +16,11 @@ buster_cfg = BusterConfig(
             """Make sure to format your answers in Markdown format, including code block and snippets. """
             """Do not include any links to urls or hyperlinks in your answers. """
             """If you do not know the answer to a question, or if it is completely irrelevant to the library usage, let the user know you cannot answer. """
+            """Use this response: """
+            """I'm sorry, but I am an AI language model trained to assist with questions related to the huggingface transformers library. I cannot answer that question as it is not relevant to the library or its usage. Is there anything else I can assist you with?"""
             """For example:\n"""
             """What is the meaning of life for huggingface?\n"""
-            """This doesn't seem to be related to the huggingface library.\n"""
             """I'm sorry, but I am an AI language model trained to assist with questions related to the huggingface transformers library. I cannot answer that question as it is not relevant to the library or its usage. Is there anything else I can assist you with?"""
-            """"""
             """Now answer the following question:\n"""
         ),
         "text_before_documents": "Only use these documents as reference:\n",
@@ -30,6 +28,7 @@ buster_cfg = BusterConfig(
             "model": "gpt-3.5-turbo",
         },
     },
+    response_format="gradio",
 )
 buster = Buster(buster_cfg)
 
--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -107,14 +107,13 @@ def test_chatbot_real_data__chatGPT():
 
 
 def test_chatbot_real_data__chatGPT_OOD():
-    hf_transformers_cfg = BusterConfig(
+    buster_cfg = BusterConfig(
         documents_file=DOCUMENTS_FILE,
         unknown_prompt="I'm sorry, but I am an AI language model trained to assist with questions related to the huggingface transformers library. I cannot answer that question as it is not relevant to the library or its usage. Is there anything else I can assist you with?",
         embedding_model="text-embedding-ada-002",
         top_k=3,
         thresh=0.7,
         max_words=3000,
-        response_format="slack",
         completer_cfg={
             "name": "ChatGPT",
             "text_before_prompt": (
@@ -122,11 +121,11 @@ def test_chatbot_real_data__chatGPT_OOD():
                 """Make sure to format your answers in Markdown format, including code block and snippets. """
                 """Do not include any links to urls or hyperlinks in your answers. """
                 """If you do not know the answer to a question, or if it is completely irrelevant to the library usage, let the user know you cannot answer. """
+                """Use this response: """
+                """I'm sorry, but I am an AI language model trained to assist with questions related to the huggingface transformers library. I cannot answer that question as it is not relevant to the library or its usage. Is there anything else I can assist you with?"""
                 """For example:\n"""
                 """What is the meaning of life for huggingface?\n"""
-                """This doesn't seem to be related to the huggingface library.\n"""
                 """I'm sorry, but I am an AI language model trained to assist with questions related to the huggingface transformers library. I cannot answer that question as it is not relevant to the library or its usage. Is there anything else I can assist you with?"""
-                """"""
                 """Now answer the following question:\n"""
             ),
             "text_before_documents": "Only use these documents as reference:\n",
@@ -134,11 +133,12 @@ def test_chatbot_real_data__chatGPT_OOD():
                 "model": "gpt-3.5-turbo",
             },
         },
+        response_format="gradio",
     )
-    buster = Buster(hf_transformers_cfg)
+    buster = Buster(buster_cfg)
     answer = buster.process_input("What is a good recipe for brocolli soup?")
     assert isinstance(answer, str)
-    assert hf_transformers_cfg.unknown_prompt in answer
+    assert buster_cfg.unknown_prompt in answer
 
 
 def test_chatbot_real_data__GPT():
