5be6cfb1196591f485063b28e14b5b7ab1042e29
Jeremy Pinto <jerpint@gmail.com>
2023-03-03 17:39:55 -0500

    isort

--- buster/completers/__init__.py
+++ buster/completers/__init__.py
@@ -1,4 +1,4 @@
-from .base import get_completer, GPT3Completer, ChatGPTCompleter
+from .base import ChatGPTCompleter, GPT3Completer, get_completer
 
 __all__ = [
     get_completer,
--- buster/completers/base.py
+++ buster/completers/base.py
@@ -1,6 +1,6 @@
-from abc import ABC, abstractmethod
 import logging
 import os
+from abc import ABC, abstractmethod
 
 import openai
 import promptlayer
