e61b318a4eb1b16a0593437ced2086cca23f4de9
Jeremy Pinto <jerpint@gmail.com>
2023-04-28 09:28:11 -0400

    Add truncation on token count and not on word count

--- buster/completers/base.py
+++ buster/completers/base.py
@@ -43,6 +43,9 @@ class Completer(ABC):
         logger.info(f"{user_input=}")
         try:
             completion = self.complete(prompt=prompt, **self.completion_kwargs)
+        except openai.error.InvalidRequestError:
+            logger.exception("Invalid request to OpenAI API. See traceback:")
+            return Completion("Something went wrong, try again soon!", True, "Invalid request made to openai.")
         except Exception as e:
             # log the error and return a generic response instead.
             logger.exception("Error connecting to OpenAI API. See traceback:")
