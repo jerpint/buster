c7c31a7279991c2d777787fc6112289a0a069d9f
Jeremy Pinto <jerpint@gmail.com>
2023-05-01 09:09:19 -0400

    update how we do formatting in system prompt

--- buster/formatters/prompts.py
+++ buster/formatters/prompts.py
@@ -15,6 +15,7 @@ class SystemPromptFormatter:
     max_tokens: 3500
     text_before_docs: str
     text_after_docs: str
+    formatter: str = "{text_before_docs}\n{documents}\n{text_after_docs}"
 
     def format(
         self,
@@ -22,8 +23,12 @@ class SystemPromptFormatter:
     ) -> str:
         """
         Prepare the system prompt with prompt engineering.
+
+        Joins the text before and after documents with
         """
-        system_prompt = self.text_before_docs + documents + self.text_after_docs
+        system_prompt = self.formatter.format(
+            text_before_docs=self.text_before_docs, documents=documents, text_after_docs=self.text_after_docs
+        )
 
         if self.tokenizer.num_tokens(system_prompt) > self.max_tokens:
             raise ValueError(f"System prompt tokens > {self.max_tokens=}")
--- tests/test_formatters.py
+++ tests/test_formatters.py
@@ -7,14 +7,12 @@ from buster.tokenizers import GPTTokenizer
 
 
 def test_documents_formatter__normal():
+    """In this test, we expect all 3 documents to be matched and returned normally."""
     tokenizer = GPTTokenizer(model_name="gpt-3.5-turbo")
     documents_formatter = DocumentsFormatter(
         tokenizer=tokenizer,
         max_tokens=100,
     )
-
-    # In this test, document_1 doesn't entirely fit.
-    # we only expect a part of it to be contained.
 
     document_1 = "This is a very short document."
     document_2 = "This is another very short document."
@@ -33,14 +31,15 @@ def test_documents_formatter__normal():
 
 
 def test_documents_formatter__doc_to_long():
+    """In this test, document_1 doesn't entirely fit.
+
+    we only expect a part of it to be contained.
+    """
     tokenizer = GPTTokenizer(model_name="gpt-3.5-turbo")
     documents_formatter = DocumentsFormatter(
         tokenizer=tokenizer,
         max_tokens=100,
     )
-
-    # In this test, document_1 doesn't entirely fit.
-    # we only expect a part of it to be contained.
 
     document_1 = "This is a very long document. It is long on purpose." * 50
     document_2 = "This is a very short document."
@@ -55,19 +54,21 @@ def test_documents_formatter__doc_to_long():
     assert len(matched_documents_new) == 1
     assert len(docs_str) < len(document_1)
 
-    # ignore the <DOCUMENT> </DOCUMENT> tags...
+    # ignore the <DOCUMENT> </DOCUMENT> tags from the formatter...
     assert docs_str[11:-11] in document_1
 
 
 def test_documents_formatter__doc_to_long_2():
+    """In this test, document_2 doesn't entirely fit.
+
+    we only expect a part of it to be contained, as well as all of document_1, and none of document_3.
+    """
+
     tokenizer = GPTTokenizer(model_name="gpt-3.5-turbo")
     documents_formatter = DocumentsFormatter(
         tokenizer=tokenizer,
         max_tokens=100,
     )
-
-    # In this test, document_2 doesn't entirely fit.
-    # we only expect a part of it to be contained.
 
     document_1 = "This is a very short document."
     document_2 = "This is a very long document. It is long on purpose." * 50
@@ -89,12 +90,18 @@ def test_documents_formatter__doc_to_long_2():
 def test_system_prompt_formatter():
     tokenizer = GPTTokenizer(model_name="gpt-3.5-turbo")
     prompt_formatter = SystemPromptFormatter(
-        tokenizer=tokenizer, max_tokens=200, text_after_docs="After docs.", text_before_docs="Before docs."
+        tokenizer=tokenizer,
+        max_tokens=200,
+        text_after_docs="After docs.",
+        text_before_docs="Before docs.",
+        formatter="{text_before_docs}\n{documents}\n{text_after_docs}",
     )
 
     documents = "Here are some docs"
 
     prompt = prompt_formatter.format(documents)
+
+    assert prompt == ("Before docs.\n" "Here are some docs\n" "After docs.")
 
     assert documents in prompt
 
@@ -102,7 +109,10 @@ def test_system_prompt_formatter():
 def test_system_prompt_formatter__to_long():
     tokenizer = GPTTokenizer(model_name="gpt-3.5-turbo")
     prompt_formatter = SystemPromptFormatter(
-        tokenizer=tokenizer, max_tokens=200, text_after_docs="After docs.", text_before_docs="Before docs."
+        tokenizer=tokenizer,
+        max_tokens=200,
+        text_after_docs="After docs.",
+        text_before_docs="Before docs.",
     )
 
     documents = "Here are some documents that are WAY too long." * 100
