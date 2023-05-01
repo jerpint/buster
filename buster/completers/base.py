a5af3f8a25038058985cd11301ee53f2348ec4a4
Jeremy Pinto <jerpint@gmail.com>
2023-05-01 08:41:13 -0400

    Better error handling (#91)
    * fix how we handle an error with a completion
    
    * check for empty string in completion

--- buster/completers/base.py
+++ buster/completers/base.py
@@ -43,10 +43,15 @@ class Completer(ABC):
         logger.info(f"{user_input=}")
         try:
             completion = self.complete(prompt=prompt, **self.completion_kwargs)
+        except openai.error.InvalidRequestError:
+            logger.exception("Error connecting to OpenAI API. See traceback:")
+            return Completion("Something went wrong, try again soon!", True, "Invalid request made to openai.")
         except Exception as e:
             # log the error and return a generic response instead.
             logger.exception("Error connecting to OpenAI API. See traceback:")
-            return Completion("", True, "We're having trouble connecting to OpenAI right now... Try again soon!")
+            return Completion(
+                "Something went wrong, try again soon!", True, "Unexpected error at the generate_response level"
+            )
 
         return Completion(completion)
 
