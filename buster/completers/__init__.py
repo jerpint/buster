5113c5877916927a6addd22c5634807065c7366b
Jeremy Pinto <jerpint@gmail.com>
2023-07-10 11:01:54 -0400

    remove methods from import

--- buster/completers/__init__.py
+++ buster/completers/__init__.py
@@ -2,13 +2,9 @@ from .base import (
     ChatGPTCompleter,
     Completer,
     Completion,
-    GPT3Completer,
-    completer_factory,
 )
 
 __all__ = [
-    completer_factory,
-    GPT3Completer,
     ChatGPTCompleter,
     Completer,
     Completion,
