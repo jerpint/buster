18d2d4c99193f652f55a661f6f00d6507876a3f0
Jeremy Pinto <jerpint@gmail.com>
2023-08-01 14:32:41 -0400

    quick refactor in cfg

--- buster/examples/cfg.py
+++ buster/examples/cfg.py
@@ -90,15 +90,17 @@ A user will submit a question. Respond 'true' if it is valid, respond 'false' if
     },
 )
 
-# initialize buster with the config in cfg.py (adapt to your needs) ...
-# buster_cfg = cfg.buster_cfg
-retriever: Retriever = SQLiteRetriever(**buster_cfg.retriever_cfg)
-tokenizer = GPTTokenizer(**buster_cfg.tokenizer_cfg)
-document_answerer: DocumentAnswerer = DocumentAnswerer(
-    completer=ChatGPTCompleter(**buster_cfg.completion_cfg),
-    documents_formatter=DocumentsFormatter(tokenizer=tokenizer, **buster_cfg.documents_formatter_cfg),
-    prompt_formatter=PromptFormatter(tokenizer=tokenizer, **buster_cfg.prompt_formatter_cfg),
-    **buster_cfg.documents_answerer_cfg,
-)
-validator: Validator = QuestionAnswerValidator(**buster_cfg.validator_cfg)
-buster: Buster = Buster(retriever=retriever, document_answerer=document_answerer, validator=validator)
+
+def setup_buster(buster_cfg: BusterConfig):
+    """initialize buster with a buster_cfg class"""
+    retriever: Retriever = SQLiteRetriever(**buster_cfg.retriever_cfg)
+    tokenizer = GPTTokenizer(**buster_cfg.tokenizer_cfg)
+    document_answerer: DocumentAnswerer = DocumentAnswerer(
+        completer=ChatGPTCompleter(**buster_cfg.completion_cfg),
+        documents_formatter=DocumentsFormatter(tokenizer=tokenizer, **buster_cfg.documents_formatter_cfg),
+        prompt_formatter=PromptFormatter(tokenizer=tokenizer, **buster_cfg.prompt_formatter_cfg),
+        **buster_cfg.documents_answerer_cfg,
+    )
+    validator: Validator = QuestionAnswerValidator(**buster_cfg.validator_cfg)
+    buster: Buster = Buster(retriever=retriever, document_answerer=document_answerer, validator=validator)
+    return buster
