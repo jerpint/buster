6714aff1efed169e4e886314500054bf7fc9ae24
Jeremy Pinto <jerpint@gmail.com>
2023-08-02 10:46:48 -0400

    add example file

---
+++ buster/examples/generate_embeddings.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from buster.documents import DeepLakeDocumentsManager
+
+# Read the csv
+df = pd.read_csv("stackoverflow.csv")
+
+# Generate the embeddings for our documents and store them in a deeplake format
+dm = DeepLakeDocumentsManager(vector_store_path="deeplake_store", overwrite=True)
+dm.add(df)
