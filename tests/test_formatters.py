a78804501ba92a06a0539a57a8630de887379af6
Jeremy Pinto <jerpint@gmail.com>
2023-09-15 13:32:30 -0400

    add tests

--- tests/test_formatters.py
+++ tests/test_formatters.py
@@ -1,12 +1,14 @@
+import json
+
 import pandas as pd
 import pytest
 
-from buster.formatters.documents import DocumentsFormatterHTML
+from buster.formatters.documents import DocumentsFormatterHTML, DocumentsFormatterJSON
 from buster.formatters.prompts import PromptFormatter
 from buster.tokenizers import GPTTokenizer
 
 
-def test_documents_formatter__normal():
+def test_DocumentsDormatterHTML__simple():
     """In this test, we expect all 3 documents to be matched and returned normally."""
     tokenizer = GPTTokenizer(model_name="gpt-3.5-turbo")
     documents_formatter = DocumentsFormatterHTML(
@@ -36,7 +38,44 @@ def test_documents_formatter__normal():
     assert docs_str == expected_docs_str
 
 
-def test_documents_formatter__doc_to_long():
+def test_DocumentsDormatterJSON__simple():
+    """In this test, we expect all 3 documents to be matched and returned normally."""
+    tokenizer = GPTTokenizer(model_name="gpt-3.5-turbo")
+    documents_formatter = DocumentsFormatterJSON(tokenizer=tokenizer, max_tokens=100, columns=["content", "source"])
+
+    document_1 = "This is a very short document."
+    document_2 = "This is another very short document."
+    document_3 = "This is also a short document."
+
+    source_1 = "source 1"
+    source_2 = "source 2"
+    source_3 = "source 3"
+
+    data_dict = {
+        "content": [document_1, document_2, document_3],
+        "source": [source_1, source_2, source_3],
+    }
+
+    expected_docs_str = json.dumps(
+        [
+            {"content": document_1, "source": source_1},
+            {"content": document_2, "source": source_2},
+            {"content": document_3, "source": source_3},
+        ],
+        separators=(",", ":"),
+    )
+
+    matched_documents = pd.DataFrame(data_dict)
+
+    docs_str, matched_documents_new = documents_formatter.format(matched_documents)
+
+    # less documents and the new document is shorter than the original
+    assert all(matched_documents.content == matched_documents_new.content)
+
+    assert docs_str == expected_docs_str  # matched_documents.to_json(orient="records")
+
+
+def test_DocumentsFormatterHTML__doc_to_long():
     """In this test, document_1 doesn't entirely fit.
 
     we only expect a part of it to be contained.
@@ -67,7 +106,54 @@ def test_documents_formatter__doc_to_long():
     assert document_3 not in docs_str
 
 
-def test_documents_formatter__doc_to_long_2():
+def test_DocumentsFormatterJSON__doc_too_long():
+    """In this test, document_3 doesn't fit.
+    We expect it to be excluded completely.
+
+    we only expect a part of it to be contained.
+    """
+    tokenizer = GPTTokenizer(model_name="gpt-3.5-turbo")
+    documents_formatter = DocumentsFormatterJSON(tokenizer=tokenizer, max_tokens=100, columns=["source", "content"])
+
+    long_sentence = "This is a very long document. It is long on purpose."
+
+    document_1 = "This is a very short document."
+    document_2 = "This is also a short document"
+    document_3 = long_sentence * 50
+
+    source_1 = "source 1"
+    source_2 = "source 2"
+    source_3 = "source 3"
+
+    data_dict = {
+        "content": [document_1, document_2, document_3],
+        "source": [source_1, source_2, source_3],
+    }
+
+    expected_docs_str = json.dumps(
+        [
+            {"content": document_1, "source": source_1},
+            {"content": document_2, "source": source_2},
+        ],
+        separators=(",", ":"),
+    )
+
+    matched_documents = pd.DataFrame(data_dict)
+
+    docs_str, matched_documents_new = documents_formatter.format(matched_documents)
+    assert docs_str == expected_docs_str
+
+    # less documents and the new document is shorter than the original
+    assert len(matched_documents) == 3
+    assert len(matched_documents_new) == 2
+
+    # The last document gets ignored completely, the first 2 make it
+    assert document_1 in docs_str
+    assert document_2 in docs_str
+    assert long_sentence not in docs_str
+
+
+def test_DocumentsFormatterHTML__doc_to_long_2():
     """In this test, document_2 doesn't entirely fit.
 
     we only expect a part of it to be contained, as well as all of document_1, and none of document_3.
@@ -96,7 +182,7 @@ def test_documents_formatter__doc_to_long_2():
     assert document_3 not in docs_str
 
 
-def test_documents_formatter__complex_format():
+def test_DocumentsFormatterHTML__complex_format():
     """In this test, we expect all 3 documents to be matched and returned in a particular format."""
     tokenizer = GPTTokenizer(model_name="gpt-3.5-turbo")
     documents_formatter = DocumentsFormatterHTML(
