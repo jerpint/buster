eea3dddf225ff477b2a600b5efe069380918fb9e
Jeremy Pinto <jerpint@gmail.com>
2023-09-21 15:18:07 -0400

    use CLI to specify the .csv filepath

--- buster/examples/generate_embeddings.py
+++ buster/examples/generate_embeddings.py
@@ -1,17 +1,26 @@
+import click
 import pandas as pd
 
 from buster.documents_manager import DeepLakeDocumentsManager
 
 REQUIRED_COLUMNS = ["url", "title", "content", "source"]
 
-# Read the csv
-df = pd.read_csv("stackoverflow.csv")
 
-# initialize our vector store from scratch
-dm = DeepLakeDocumentsManager(vector_store_path="deeplake_store", overwrite=True, required_columns=REQUIRED_COLUMNS)
+@click.command()
+@click.option("--csv", default="stackoverflow.csv", help="Path to the CSV file.")
+def main(csv):
+    # Read the csv
+    df = pd.read_csv(csv)
 
-# Generate the embeddings for our documents and store them to the deeplake store
-dm.add(df, csv_filename="embeddings.csv")
+    # initialize our vector store from scratch
+    dm = DeepLakeDocumentsManager(vector_store_path="deeplake_store", overwrite=True, required_columns=REQUIRED_COLUMNS)
 
-# Save it to a zip file
-dm.to_zip()
+    # Generate the embeddings for our documents and store them to the deeplake store
+    dm.add(df, csv_filename="embeddings.csv")
+
+    # Save it to a zip file
+    dm.to_zip()
+
+
+if __name__ == "__main__":
+    main()
