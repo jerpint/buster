ef356b43905d8fa287d2dff29e418f75cdbaf163
Jeremy Pinto <jerpint@gmail.com>
2023-11-03 15:53:13 -0400

    add question reformatter

---
+++ buster/formatters/questions.py
@@ -0,0 +1,20 @@
+from buster.completers import ChatGPTCompleter
+import logging
+
+class QuestionReformulator:
+    def __init__(self):
+        completion_kwargs = {
+            "model": "gpt-3.5-turbo",
+            "stream": False,
+            "temperature": 0,
+        }
+        self.completer = ChatGPTCompleter(completion_kwargs=completion_kwargs)
+        self.system_prompt = """
+        Your role is to reformat a user's input into a question that is useful in the context of a semantic retrieval system.
+        Reformulate the question in a way that captures the original essence of the question while also adding more relevant details that can be useful in the context of semantic retrieval."""
+
+    def reformulate(self, user_input: str) -> str:
+        """Reformulate a user's question"""
+        reformulated_question, error = self.completer.complete(self.system_prompt, user_input=user_input)
+        logging.info(f"Reformulated question from {user_input=} to {reformulated_question=}")
+        return reformulated_question, error
